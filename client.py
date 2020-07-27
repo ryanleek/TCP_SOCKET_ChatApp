f0b808c44191ae7410a3bba2ab46c9abe7c8b167
ryanleek <30751720+ryanleek@users.noreply.github.com>
2020-07-27 16:01:20 +0900

    Update client.py, server.py
    Changed from echo server-client to a messaged based server-client.
    Client now sends message with a header that shows message length.

--- client.py
+++ client.py
@@ -1,22 +1,17 @@
 import socket
-import sys
 
+HEADERSIZE = 10
 sck = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 svr_address = ('localhost', 10000)
 sck.connect(svr_address)
 
 try:
-    message = input()
-    sck.sendall(message)
-
-    received = 0
-    expect = len(message)
-
-    while received < expect:
-        data = sck.recv(16)
-        received += len(data)
-        print('received', data)
+    msg = input()
+    #add messge length as size 10 header to msg
+    msg = f"{len(msg):<{HEADERSIZE}}" + msg
+    #send as bytes
+    sck.send(bytes(msg,"utf-8"))
 
 finally:
     sck.close()
--- server.py
+++ server.py
@@ -1,11 +1,13 @@
 import socket
 
+HEADERSIZE = 10
 sck = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #setting address & port no.
 svr_address = ('localhost', 10000)
 sck.bind(svr_address)
 
+#socket goes to server mode
 sck.listen(1)
 
 while True:
@@ -15,16 +17,22 @@ while True:
     try:
         print('connection from', clt_address)
 
+        full_data = ''
+        new_data = True
+
         while True:
             data = connection.recv(16)
-            print('received', data)
-            if data:
-                connection.sendall(data)
-            else:
-                print('no more data')
-                break
+
+            if new_data:
+                data_len = int(data[:HEADERSIZE])
+                new_data = False
+            
+            full_data += data.decode("utf-8")
+
+            if len(full_data)-HEADERSIZE == data_len:
+                print("full message:", full_data[HEADERSIZE:])
+                new_data = True
     
     finally:
+        #clean up connection
         connection.close()
-
-
